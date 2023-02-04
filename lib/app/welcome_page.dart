e84921b4c6185d16131f6ca846bbd8fbf589c9fa
Plutonchi <boburbilol2016@gmail.com>
2023-02-04 16:58:56 +0600

    welcome_page

--- lib/app/welcome_page.dart
+++ lib/app/welcome_page.dart
@@ -7,7 +7,7 @@ class WelcomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Welcome Page"),
+        title: const Text("Welcome Page _"),
       ),
     );
   } 
