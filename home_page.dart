31687a6d0eda0dd8616483a201cc48c16e2429d8
Mukhammadaliti <115714076+Mukhammadaliti@users.noreply.github.com>
2023-02-04 17:00:21 +0600

    Add files via upload

---
+++ home_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          color: Colors.blue,
+          width: 50,
+          height: 50,
+          child: Text('Hello'),
+        ),
+      ),
+    );
+  }
+}
